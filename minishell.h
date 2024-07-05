26f1d45993039f179252a61efe49655553f6d34f
Silva5525 <132822583+Silva5525@users.noreply.github.com>
2024-07-05 15:49:16 +0200

    start

---
+++ minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wdegraf <wdegraf@student.42heilbronn.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/05 14:52:57 by wdegraf           #+#    #+#             */
+/*   Updated: 2024/07/05 15:37:52 by wdegraf          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+#define MINISHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+#include "../libft/libft.h"
+
+#endif
