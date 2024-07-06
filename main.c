65237da87be4f804d33f3ca17fd87c904958660c
Silva5525 <132822583+Silva5525@users.noreply.github.com>
2024-07-06 12:35:24 +0200

    test of char **envp

--- main.c
+++ main.c
@@ -6,14 +6,33 @@
 /*   By: wdegraf <wdegraf@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/05 15:38:45 by wdegraf           #+#    #+#             */
-/*   Updated: 2024/07/05 15:54:19 by wdegraf          ###   ########.fr       */
+/*   Updated: 2024/07/06 12:34:26 by wdegraf          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	main(void)
+/// @brief 
+/// @param argc Argument Count
+/// @param argv Argument Vector
+/// @param envp Environment Pointer (each string is a enviroment variable)
+/// @return 
+int	main(int argc, char **argv, char **envp)
 {
+	int i;
 	
+	i = 0;
+	printf("argc: %d\n", argc);
+	while (argv[i])
+	{
+		printf("argv[%d]: %s\n", i, argv[i]);
+		i++;
+	}
+	i = 0;
+	while (envp[i])
+	{
+		printf("envp[%d]: %s\n", i, envp[i]);
+		i++;
+	}
 	return(0);
 }
